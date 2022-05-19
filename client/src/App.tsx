46f6c489d54802f38eadd8deeff7dfd96bfb1a34
Jay van Wyk <jayvanwyk@gmail.com>
2022-05-19 13:22:00 +0100

    Added initial routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import JoinRoomPage from './app/JoinRoomPage/JoinRoomPage';
+import RoomPage from './app/RoomPage/RoomPage';
+import IntroductionPage from './app/IntroductionPage/IntroductionPage';
 
 function App() {
   return (
-    <div className="App">
-      Here is the cleaned up app
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/join-room">
+          <JoinRoomPage/>
+        </Route>
+        <Route path="/room">
+          <RoomPage/>
+        </Route>
+        <Route path="/">
+          <IntroductionPage/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
