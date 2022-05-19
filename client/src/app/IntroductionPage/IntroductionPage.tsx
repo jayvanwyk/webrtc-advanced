4ebd7537c9fc8d2390ba0f03def52efa690750c1
Jay van Wyk <jayvanwyk@gmail.com>
2022-05-19 13:12:39 +0100

    Created initial components

---
+++ client/src/app/IntroductionPage/IntroductionPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IntroductionPage = () =>{
+    return (
+        <div>
+            Introduction Page Component
+        </div>
+    )
+}
+
+export default IntroductionPage;
---
+++ client/src/app/JoinRoomPage/JoinRoomPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const JoinRoomPage = () =>{
+    return (
+        <div>
+            Join Room Page
+        </div>
+    )
+};
+
+export default JoinRoomPage;
